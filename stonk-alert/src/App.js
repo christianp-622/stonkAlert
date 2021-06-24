a2da5fac874f50524cc996f2be217e9a6f9c2f94
Kevin Yang <kyang2001@yahoo.com>
2021-06-24 03:47:34 -0500

    Added more pages that represent each model/company in the companies page

--- stonk-alert/src/App.js
+++ stonk-alert/src/App.js
@@ -6,9 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import {Stocks, Stock_Page,Stock_Table} from './pages/Stocks';
+import {Stocks, Stock_Page, Stock_Table} from './pages/Stocks';
 import News from './pages/News';
-import Companies from './pages/Companies';
+import {Companies, GME_Page, AMC_Page, BlackBerry_Page} from './pages/Companies';
 
 function App() {
   return (
@@ -16,7 +16,10 @@ function App() {
           <Switch>
             <Route path="/" component={Home} exact/>
             <Route path="/about" component={About}/>
-            <Route path="/companies" component={Companies}/>
+            <Route path="/companies" component={Companies} exact/>
+            <Route path="/companies/GME" component={GME_Page}/>
+            <Route path="/companies/AMC" component={AMC_Page}/>
+            <Route path="/companies/BB" component={BlackBerry_Page}/>
             <Route path="/stocks" component={Stocks} exact/>
             <Route path="/stocks/:id" component={Stock_Page}/>  brb remove after experiment with table
             <Route path="/news" component={News}/>
