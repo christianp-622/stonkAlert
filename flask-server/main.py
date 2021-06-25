3e7f7c8ce8dee461794f36cce36d752a0bebdf79
Albert Cho <albert.cho.kor@gmail.com>
2021-06-24 18:43:22 -0700

    Edited main.py file for GCP.

--- flask-server/main.py
+++ flask-server/main.py
@@ -6,4 +6,5 @@ app = Flask("__main__")
 def my_index():
     return render_template("index.html", flask_token="Hello   world")
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
