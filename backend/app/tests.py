47b6da2aecba38a17da5d40baada93065d875b31
christianp-622 <christianp62200@gmail.com>
2021-07-07 18:49:48 -0600

    Added commentary to describe the purpose of each unit test

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -9,11 +9,11 @@ api_base = "http://localhost:3000/api"
 
 
 class tests(TestCase):
-    # Company API Test
+    # Company Model API Test
     company_response= requests.get("{}/company".format(api_base))
     company_json = company_response.json()
 
-    # test response attribute of Company model
+    # test response of get request for the Company model 
     def test_company_response(self):
         self.assertEqual(self.article_response.status_code, 200)
 
@@ -64,68 +64,89 @@ class tests(TestCase):
         self.assertTrue("tradescore" in stock.keys())
         self.assertTrue("marketcap" in stock.keys())
 
-    # Stock API Test 
+
+
+    # Stock Model API Test 
     stock_response= requests.get("{}/stock".format(api_base))
     stock_json = stock_response.json()
 
+    # test response of get request for Stock model
     def test_stock_response(self):
         self.assertEqual(self.article_response.status_code, 200)
 
+    # test ticker attribute of Stock model
     def test_stock_ticker(self):
         self.assertTrue("ticker" in self.stock_json.keys())
     
+    # test company attribute of Stock model
     def test_stock_company(self):
         self.assertTrue("company" in self.stock_json.keys())
 
+    # test price attribute of Stock model
     def test_stock_price(self):
         self.assertTrue("price" in self.stock_json.keys())
 
+    # test sector attribute of Stock model
     def test_stock_sector(self):
         self.assertTrue("sector" in self.stock_json.keys())
 
+    # test tradescore attribute of Stock model
     def test_stock_tradescore(self):
         self.assertTrue("tradescore" in self.stock_json.keys())
 
+    # test investscore attribute of Stock model
     def test_stock_investscore(self):
         self.assertTrue("investscore" in self.stock_json.keys())
 
+    # test marketcap attribute of Stock model
     def test_stock_marketcap(self):
         self.assertTrue("marketcap" in self.stock_json.keys())
 
 
-    # Articles API Test
+
+    # Articles Model API Test
     article_response= requests.get("{}/article".format(api_base))
     article_json = article_response.json()
 
+    # test response of get request for Article model
     def test_article_response(self):
         self.assertEqual(self.article_response.status_code, 200)
 
+    # test id attribute of Article model
     def test_article_id(self):
         self.assertTrue("id" in self.article_json.keys())
 
+    # test headline attribute of Article model
     def test_article_headline(self):
         self.assertTrue("headline" in self.article_json.keys())
 
+    # test datetime attribute of Article model
     def test_article_datetime(self):
         self.assertTrue("datetime" in self.article_json.keys())
 
+    # test image attribute of Article model
     def test_article_image(self):
         self.assertTrue("image" in self.article_json.keys())
 
+    # test source attribute of Article model
     def test_article_source(self):
         self.assertTrue("source" in self.article_json.keys())
 
+    # test link attribute of Article model
     def test_article_link(self):
         self.assertTrue("link" in self.article_json.keys())
 
+    # test summary attribute of Article model
     def test_article_summary(self):
         self.assertTrue("summary" in self.article_json.keys())
 
+    # test ticker attribute of Article model
     def test_article_ticker(self):
         self.assertTrue("ticker" in self.article_json.keys())
     
+    # test company attribute of Article model
     def test_article_ticker(self):
-        self.assertTrue("company" in self.article_jason.keys())
+        self.assertTrue("company" in self.article_json.keys())
 
 
 
